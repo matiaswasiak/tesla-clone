5c107eaadad18d2646e1adf9e940a547ed044c9b
Matias Wasiak <matiaswasiak@gmail.com>
2022-06-29 22:46:17 -0300

    first setup with images and starting the project

---
+++ src/components/Section.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return <Wrap>Section</Wrap>;
+}
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-image: url("/images/model-s.jpg");
+`;
+
+export default Section;
