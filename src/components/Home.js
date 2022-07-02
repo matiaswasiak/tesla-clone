e5efc65caef6e16f8178231f10eb0cd1de5cbe83
Matias Wasiak <matiaswasiak@gmail.com>
2022-07-01 22:44:14 -0300

    cleaning files

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,13 +15,6 @@ function Home() {
         rightBtnText="Existing inventory"
       />
       <Section
-        title="Model Y"
-        description="Order Online for Touchless Delivery"
-        backgroundImg="model-y.jpg"
-        leftBtnText="Custom order"
-        rightBtnText="Existing inventory"
-      />
-      <Section
         title="Model 3"
         description="Order Online for Touchless Delivery"
         backgroundImg="model-3.jpg"
@@ -32,6 +25,13 @@ function Home() {
         title="Model X"
         description="Order Online for Touchless Delivery"
         backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
         leftBtnText="Custom order"
         rightBtnText="Existing inventory"
       />
